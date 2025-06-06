932eac357346462782207caceb3507aa16c70cde
XaazaaaX <benito.ernst@t-online.de>
2025-06-06 14:09:11 +0200

    add tests for pipline

---
+++ DlsaApi/src/test/java/de/dlsa/test/CalculatorTest.java
@@ -0,0 +1,14 @@
+package de.dlsa.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        //Calculator calc = new Calculator();
+        assertEquals(5, 5);
+    }
+}
